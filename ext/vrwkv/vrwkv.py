72bc7c79afe792acc757baa90b5e7c7c63c01fa6
Jonathan Bonilla <jonathanbonilla210@gmail.com>
2025-01-22 15:10:02 -0500

    - Fixed utils import in vrwkv.py - Fixed cuda code import location

--- ext/vrwkv/vrwkv.py
+++ ext/vrwkv/vrwkv.py
@@ -14,13 +14,13 @@ import torch.utils.checkpoint as cp
 # from mmcv.cnn.bricks.transformer import PatchEmbed
 # from mmseg.models.builder import BACKBONES
 # from mmengine.logging import MMLogger
-from utils import resize_pos_embed, DropPath
+from ext.vrwkv.utils import resize_pos_embed, DropPath
 
 T_MAX = 8192 # increase this if your ctx_len is long [NOTE: TAKES LOTS OF VRAM!]
 # it's possible to go beyond CUDA limitations if you slice the ctx and pass the hidden state in each slice
 
 from torch.utils.cpp_extension import load
-wkv_cuda = load(name="wkv", sources=["cuda/wkv_op.cpp", "cuda/wkv_cuda.cu"],
+wkv_cuda = load(name="wkv", sources=["ext/vrwkv/cuda/wkv_op.cpp", "ext/vrwkv/cuda/wkv_cuda.cu"],
                 verbose=True, extra_cuda_cflags=['-res-usage', '--maxrregcount 60', '--use_fast_math', '-O3', '-Xptxas -O3', f'-DTmax={T_MAX}'])
 
 def get_logger(name=__name__):
