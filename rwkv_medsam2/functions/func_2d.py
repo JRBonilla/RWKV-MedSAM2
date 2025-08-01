886cea622b699671d34ea07d637329619036c690
Jonathan Bonilla <jonathanbonilla210@gmail.com>
2025-08-01 02:52:15 -0400

    Attempt to fix type mismatch in adapters #11.79

--- rwkv_medsam2/functions/func_2d.py
+++ rwkv_medsam2/functions/func_2d.py
@@ -186,7 +186,9 @@ def train_step_2d(student, teacher, optimizer, batch, config, memory_bank, scale
                 points=(sparse_points, sparse_labels) if sparse_points is not None else None,
                 boxes=None, masks=None
             )
-            teacher_dense_embs = F.interpolate(teacher_dense_embs, size=teacher_embed.shape[-2:], mode='bilinear', align_corners=False)
+
+            proj = nn.Conv2d(256, 1024, kernel_size=1).to(image_embed.device)
+            teacher_dense_embs = proj(teacher_dense_embs)
             teacher_image_pe   = teacher.sam_prompt_encoder.get_dense_pe()
             
             print(f"Teacher image embedding: {teacher_embed.shape}, teacher dense embs: {teacher_dense_embs.shape}")
