c8b855afff916ac2b214527d21e4f0f3744d2182
Jonathan Bonilla <jonathanbonilla210@gmail.com>
2025-08-01 01:05:55 -0400

    Use built-in high-res adapters in mask decoder

--- rwkv_medsam2/functions/func_2d.py
+++ rwkv_medsam2/functions/func_2d.py
@@ -154,6 +154,7 @@ def train_step_2d(student, teacher, optimizer, batch, config, memory_bank, scale
         image_embed = feats[-1]
         raw_feat_hr, raw_feat_mr = feats[:-1] # [feat_hr, feat_mr]
 
+        # Use built-in high-res adapters in mask decoder
         hires_feats = [
             student.sam_mask_decoder.conv_s0(raw_feat_hr),
             student.sam_mask_decoder.conv_s1(raw_feat_mr)
